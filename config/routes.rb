40f846e95550af0635e3229da47188b47f421eb4
KAZUKI OMOTO <k1bbs2str@gmail.com>
2019-04-09 03:15:53 +0000

    Capter2_2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-
   root 'home#top'
+  get 'top', to: 'home#top'
   get 'about', to: 'home#about'
+  get 'posts/index', to: 'posts#index'
+
+  
+  
 end
