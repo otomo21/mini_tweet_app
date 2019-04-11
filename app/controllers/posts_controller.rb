58dbd0b30a489e54277b20d46faa1fec400824b1
KAZUKI OMOTO <k1bbs2str@gmail.com>
2019-04-11 05:15:55 +0000

    Capter9

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user
+  before_action :limitation_corrent_user, only: [:edit, :update, :destroy]
   
   def index
     @posts = Post.all.order(created_at: :desc)
@@ -7,6 +8,7 @@ class PostsController < ApplicationController
   
   def show
     @post = Post.find(params[:id])
+    @user = @post.user
   end
   
   def new
@@ -14,7 +16,9 @@ class PostsController < ApplicationController
   end
   
   def create
-    @post = Post.new(content: params[:content])
+    @post = Post.new(
+      content: params[:content],
+      user_id: @current_user.id,)
     if @post.save
       flash[:notice] = "投稿しました！"
       redirect_to posts_index_url
@@ -25,11 +29,9 @@ class PostsController < ApplicationController
   end
   
   def edit
-    @post = Post.find(params[:id])
   end
   
   def update
-    @post = Post.find(params[:id])
     @post.content = params[:content]
     if @post.save
       flash[:notice] = "投稿を編集しました。"
@@ -40,9 +42,16 @@ class PostsController < ApplicationController
   end
   
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     flash[:notice] = "投稿を削除しました。"
     redirect_to posts_index_url
+  end
+  
+  def limitation_corrent_user
+    @post = Post.find(params[:id])
+    unless @post.user_id == @current_user.id
+      flash[:notice] = "自分以外のユーザーの投稿は編集できません。"
+      redirect_to posts_index_url
+    end
   end
 end
